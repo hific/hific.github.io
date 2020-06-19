eed887a167c0475e6a7470073e86d12484a30a68
Fabian Mentzer <fmentzer@me.com>
2020-06-19 21:39:30 +0200

    Release

---
+++ script.js
@@ -0,0 +1,366 @@
+let known = {
+'food': {
+    'imginfo': 'DIV2K/0848.png',
+    'fullres': 'https://storage.googleapis.com/hific/div2k/visualize.html?perPage=10&imgs=0848',
+    'loadingRatio': '75.29411764705883%',
+    'left': ['food_HiFiC_Lo.png', 'HiFiC (Ours): 0.155bpp', 'HiFiC (Ours): 61 kB'],
+    'right': [
+        ['food_jpg_1x_0.165.jpg', 'JPG (0.165 bpp, \u22481x ours kB)', 'JPG (65 kB, \u22481x ours kB)'],
+        ['food_jpg_2x_0.312.jpg', 'JPG (0.312 bpp, \u22482x)', 'JPG (122 kB, \u22482x)'],
+        ['food_jpg_3x_0.479.jpg', 'JPG (0.479 bpp, \u22483x)', 'JPG (188 kB, \u22483x)'],
+        ['food_jpg_4x_0.624.jpg', 'JPG (0.624 bpp, \u22484x)', 'JPG (245 kB, \u22484x)'],
+        ['DIVIDER'],
+        ['food_bpg_1x_0.168.png', 'BPG (0.168 bpp, \u22481x)', 'BPG (66 kB, \u22481x)'],
+        ['food_bpg_2x_0.345.png', 'BPG (0.345 bpp, \u22482x)', 'BPG (135 kB, \u22482x)'],
+        ['food_bpg_3x_0.541.png', 'BPG (0.541 bpp, \u22483x)', 'BPG (212 kB, \u22483x)'],
+        ['DIVIDER'],
+        ['food_originals.png', 'Original', 'Original'],
+    ],
+    'thumb': 'food_thumb.jpg'
+},
+'shades': {
+    'imginfo': 'CLIC2020/ad249',
+    'fullres': 'https://storage.googleapis.com/hific/clic2020/visualize.html?perPage=10&imgs=ad249bba099568403dc6b97bc37f8d74',
+    'loadingRatio': '71.240234375%',
+    'left': ['shades_HiFiC_Lo.png', 'HiFiC (Ours): 0.198bpp', 'HiFiC (Ours): 74 kB'],
+    'right': [
+        ['shades_jpg_1x_0.209.jpg', 'JPG (0.209 bpp, \u22481x ours kB)', 'JPG (78 kB, \u22481x ours kB)'],
+        ['shades_jpg_2x_0.404.jpg', 'JPG (0.404 bpp, \u22482x)', 'JPG (151 kB, \u22482x)'],
+        ['shades_jpg_3x_0.596.jpg', 'JPG (0.596 bpp, \u22483x)', 'JPG (223 kB, \u22483x)'],
+        ['shades_jpg_4x_0.807.jpg', 'JPG (0.807 bpp, \u22484x)', 'JPG (302 kB, \u22484x)'],
+        ['DIVIDER'],
+        ['shades_bpg_1x_0.224.png', 'BPG (0.224 bpp, \u22481x)', 'BPG (84 kB, \u22481x)'],
+        ['shades_bpg_2x_0.446.png', 'BPG (0.446 bpp, \u22482x)', 'BPG (167 kB, \u22482x)'],
+        ['shades_bpg_3x_0.659.png', 'BPG (0.659 bpp, \u22483x)', 'BPG (246 kB, \u22483x)'],
+        ['DIVIDER'],
+        ['shades_originals.png', 'Original', 'Original'],
+    ],
+    'thumb': 'shades_thumb.jpg'
+},
+'tower': {
+    'imginfo': 'CLIC2020/9cbf2',
+    'fullres': 'https://storage.googleapis.com/hific/clic2020/visualize.html?perPage=10&imgs=9cbf2594f339c0d3d0f0ea25c62af52b',
+    'loadingRatio': '66.650390625%',
+    'left': ['tower_HiFiC_Lo.png', 'HiFiC (Ours): 0.116bpp', 'HiFiC (Ours): 41 kB'],
+    'right': [
+        ['tower_jpg_1x_0.140.jpg', 'JPG (0.140 bpp, \u22481x ours kB)', 'JPG (49 kB, \u22481x ours kB)'],
+        ['tower_jpg_2x_0.236.jpg', 'JPG (0.236 bpp, \u22482x)', 'JPG (82 kB, \u22482x)'],
+        ['tower_jpg_3x_0.357.jpg', 'JPG (0.357 bpp, \u22483x)', 'JPG (125 kB, \u22483x)'],
+        ['tower_jpg_4x_0.470.jpg', 'JPG (0.470 bpp, \u22484x)', 'JPG (164 kB, \u22484x)'],
+        ['DIVIDER'],
+        ['tower_bpg_1x_0.141.png', 'BPG (0.141 bpp, \u22481x)', 'BPG (49 kB, \u22481x)'],
+        ['tower_bpg_2x_0.256.png', 'BPG (0.256 bpp, \u22482x)', 'BPG (89 kB, \u22482x)'],
+        ['tower_bpg_3x_0.361.png', 'BPG (0.361 bpp, \u22483x)', 'BPG (126 kB, \u22483x)'],
+        ['DIVIDER'],
+        ['tower_originals.png', 'Original', 'Original'],
+    ],
+    'thumb': 'tower_thumb.jpg'
+},
+'paris': {
+    'imginfo': 'CLIC2020/1ac06',
+    'fullres': 'https://storage.googleapis.com/hific/clic2020/visualize.html?perPage=10&imgs=1ac06ad4b53f9880698aaaaa730363c6',
+    'loadingRatio': '75.0%',
+    'left': ['paris_HiFiC_Lo.png', 'HiFiC (Ours): 0.172bpp', 'HiFiC (Ours): 66 kB'],
+    'right': [
+        ['paris_jpg_1x_0.180.jpg', 'JPG (0.180 bpp, \u22481x ours kB)', 'JPG (69 kB, \u22481x ours kB)'],
+        ['paris_jpg_2x_0.359.jpg', 'JPG (0.359 bpp, \u22482x)', 'JPG (137 kB, \u22482x)'],
+        ['paris_jpg_3x_0.517.jpg', 'JPG (0.517 bpp, \u22483x)', 'JPG (197 kB, \u22483x)'],
+        ['paris_jpg_4x_0.698.jpg', 'JPG (0.698 bpp, \u22484x)', 'JPG (266 kB, \u22484x)'],
+        ['DIVIDER'],
+        ['paris_bpg_1x_0.177.png', 'BPG (0.177 bpp, \u22481x)', 'BPG (67 kB, \u22481x)'],
+        ['paris_bpg_2x_0.357.png', 'BPG (0.357 bpp, \u22482x)', 'BPG (136 kB, \u22482x)'],
+        ['paris_bpg_3x_0.566.png', 'BPG (0.566 bpp, \u22483x)', 'BPG (216 kB, \u22483x)'],
+        ['DIVIDER'],
+        ['paris_originals.png', 'Original', 'Original'],
+    ],
+    'thumb': 'paris_thumb.jpg'
+},
+'bus': {
+    'imginfo': 'CLIC2020/4051a',
+    'fullres': 'https://storage.googleapis.com/hific/clic2020/visualize.html?perPage=10&imgs=4051aeed928b0258553e2e6c7ce36466',
+    'loadingRatio': '66.650390625%',
+    'left': ['bus_HiFiC_Lo.png', 'HiFiC (Ours): 0.072bpp', 'HiFiC (Ours): 25 kB'],
+    'right': [
+        ['bus_jpg_2x_0.146.jpg', 'JPG (0.146 bpp, \u22482x)', 'JPG (51 kB, \u22482x)'],
+        ['bus_jpg_3x_0.218.jpg', 'JPG (0.218 bpp, \u22483x)', 'JPG (76 kB, \u22483x)'],
+        ['bus_jpg_4x_0.288.jpg', 'JPG (0.288 bpp, \u22484x)', 'JPG (101 kB, \u22484x)'],
+        ['DIVIDER'],
+        ['bus_bpg_1x_0.073.png', 'BPG (0.073 bpp, \u22481x)', 'BPG (26 kB, \u22481x)'],
+        ['bus_bpg_2x_0.162.png', 'BPG (0.162 bpp, \u22482x)', 'BPG (57 kB, \u22482x)'],
+        ['bus_bpg_3x_0.224.png', 'BPG (0.224 bpp, \u22483x)', 'BPG (78 kB, \u22483x)'],
+        ['DIVIDER'],
+        ['bus_originals.png', 'Original', 'Original'],
+    ],
+    'thumb': 'bus_thumb.jpg'
+},
+'plant': {
+    'imginfo': 'CLIC2020/36720',
+    'fullres': 'https://storage.googleapis.com/hific/clic2020/visualize.html?perPage=10&imgs=36720dbea875d6f7f81ade5b42eef871',
+    'loadingRatio': '66.650390625%',
+    'left': ['plant_HiFiC_Lo.png', 'HiFiC (Ours): 0.135bpp', 'HiFiC (Ours): 47 kB'],
+    'right': [
+        ['plant_jpg_1x_0.145.jpg', 'JPG (0.145 bpp, \u22481x ours kB)', 'JPG (51 kB, \u22481x ours kB)'],
+        ['plant_jpg_2x_0.278.jpg', 'JPG (0.278 bpp, \u22482x)', 'JPG (97 kB, \u22482x)'],
+        ['plant_jpg_3x_0.407.jpg', 'JPG (0.407 bpp, \u22483x)', 'JPG (142 kB, \u22483x)'],
+        ['plant_jpg_4x_0.544.jpg', 'JPG (0.544 bpp, \u22484x)', 'JPG (190 kB, \u22484x)'],
+        ['DIVIDER'],
+        ['plant_bpg_1x_0.156.png', 'BPG (0.156 bpp, \u22481x)', 'BPG (54 kB, \u22481x)'],
+        ['plant_bpg_2x_0.272.png', 'BPG (0.272 bpp, \u22482x)', 'BPG (95 kB, \u22482x)'],
+        ['plant_bpg_3x_0.448.png', 'BPG (0.448 bpp, \u22483x)', 'BPG (157 kB, \u22483x)'],
+        ['DIVIDER'],
+        ['plant_originals.png', 'Original', 'Original'],
+    ],
+    'thumb': 'plant_thumb.jpg'
+},
+'wall': {
+    'imginfo': 'Kodak/kodim02.png',
+    'fullres': 'https://storage.googleapis.com/hific/kodak/visualize.html?perPage=10&imgs=kodim02',
+    'loadingRatio': '66.66666666666666%',
+    'left': ['wall_HiFiC_Lo.png', 'HiFiC (Ours): 0.143bpp', 'HiFiC (Ours): 7 kB'],
+    'right': [
+        ['wall_jpg_1x_0.150.jpg', 'JPG (0.150 bpp, \u22481x ours kB)', 'JPG (7 kB, \u22481x ours kB)'],
+        ['wall_jpg_2x_0.290.jpg', 'JPG (0.290 bpp, \u22482x)', 'JPG (14 kB, \u22482x)'],
+        ['wall_jpg_3x_0.441.jpg', 'JPG (0.441 bpp, \u22483x)', 'JPG (22 kB, \u22483x)'],
+        ['wall_jpg_4x_0.576.jpg', 'JPG (0.576 bpp, \u22484x)', 'JPG (28 kB, \u22484x)'],
+        ['DIVIDER'],
+        ['wall_bpg_1x_0.158.png', 'BPG (0.158 bpp, \u22481x)', 'BPG (8 kB, \u22481x)'],
+        ['wall_bpg_2x_0.336.png', 'BPG (0.336 bpp, \u22482x)', 'BPG (17 kB, \u22482x)'],
+        ['DIVIDER'],
+        ['wall_originals.png', 'Original', 'Original'],
+    ],
+    'thumb': 'wall_thumb.jpg'
+},
+};
+/// INPUT_DATA_END
+
+let PREFIX = 'https://storage.googleapis.com/hific/data/img/';
+let order = ['shades', 'tower', 'wall', 'food', 'bus', 'plant', 'paris'];
+let captionTypeToIndex = {'bpp': 1, 'bytes': 2};
+let captionIndex = captionTypeToIndex['bytes'];
+let currentImage = null;
+
+$(document).ready(function () {
+    $('input[type=radio][name=bpp-bytes-radio]').change(function() {
+        captionIndex = captionTypeToIndex[this.value];
+        showImgAndUpdateUI(currentImage, true);
+    });
+    $('#toggle-aff-footnote').on('click', function (e) {
+        $('#footnote-aff').toggle();
+    });
+    $("#view-full-res").hover(
+        function() {
+            let title = $(this).attr("data-title");
+            $('<div/>', {
+                text: title,
+                class: 'overlay-box'
+            }).appendTo(this);
+        }, function() {
+            $(document).find("div.overlay-box").remove();
+        }
+    );
+    if ($(".comparison-slider")[0]) {
+        let compSlider = $(".comparison-slider");
+        compSlider.each(function () {
+            let compSliderWidth = $(this).width() + "px";
+            $(this).find(".resize img").css({width: compSliderWidth});
+            drags($(this).find(".divider"), $(this).find(".resize"), $(this));
+        });
+        $(window).on("resize", function () {
+            let compSliderWidth = compSlider.width() + "px";
+            compSlider.find(".resize img").css({width: compSliderWidth});
+        });
+        $('.image-selector').empty();
+        order.forEach(function (v, index) {
+            let thumbID = "img-sel-" + index.toString();
+            let img =
+                $('<img>', {
+                    'id': "img-sel-" + index.toString(),
+                    'class': "thumb" + (index === 0 ? " thumb-active" : "") + " " + thumbID,
+                    'src': PREFIX + known[v]['thumb']});
+            img.on('click', function (e) {
+                let selectedThumb = e.target;
+                let selectedThumbClasses = '.' + selectedThumb.className.split(' ').join('.');
+                $('.thumb.thumb-active').removeClass('thumb-active');
+                $(selectedThumbClasses).addClass('thumb-active');
+                $(selectedThumb).addClass('thumb-active');
+                let imgSelId = selectedThumb.id;
+                let index = parseInt(imgSelId.replace("img-sel-", ""));
+                console.log(index);
+                let imgName = order[index];
+                showImgAndUpdateUI(imgName, false);
+            });
+            $('.image-selector').append(img);
+        });
+        showImgAndUpdateUI(order[0], false);
+    }
+});
+
+function showLeftImg(img, imgInfo, fullres, loadingRatio) {
+    let imgName = img[0];
+    let img_caption = img[captionIndex];
+    $('#left-info-button').text(img_caption);
+    $('#img-info-button').text(imgInfo);
+    $('#view-full-res').attr('href', fullres);
+    let leftImg = $('#left')[0];
+    let showing = leftImg.src.split('/').reverse()[0];
+    if (showing === imgName) {
+        console.log('Left already on', imgName);
+        return;
+    }
+    leftImg.src = PREFIX + imgName;
+    if (leftImg.complete) {
+        return;
+    }
+    let rightImgs = $('#right-imgs'); rightImgs.hide();
+    $(leftImg).hide();
+    let loadingPlaceholder = $('#loading-placeholder');
+    loadingPlaceholder.css('display', 'block');
+    loadingPlaceholder.css('padding-top', loadingRatio);
+    function loaded() {
+        $(leftImg).show();
+        rightImgs.show();
+        loadingPlaceholder.css('display', 'none');
+        removeListeners();
+    }
+    function error() { console.log('Error when loading', newLeft); removeListeners(); }
+    function removeListeners() {
+        leftImg.removeEventListener('load', loaded);
+        leftImg.removeEventListener('error', error);
+    }
+    removeListeners();
+    leftImg.addEventListener('load', loaded);
+    leftImg.addEventListener('error', error);
+}
+
+function showRights(imgName, imgs, force) {
+    let rightsDiv = $('#right-imgs')[0];
+    if (!force && rightsDiv.getAttribute("showing") === imgName) {
+        console.log("Right already on", imgName);
+        return;
+    }
+    let rightSelector = $(".right-selector")[0];
+    $(rightSelector).empty();
+    $(rightsDiv).empty();
+    rightsDiv.setAttribute("showing", imgName);
+    console.log(imgs, rightsDiv.getAttribute("showing"));
+    let buttonGroupDiv = $("<div>", {'class': 'btn-group-vertical btn-group-sm', 'role': 'group'});
+    $(rightSelector).append(buttonGroupDiv);
+    imgs.forEach(function (item, index) {
+        let cls = ["btn", "float-right", "right-sel-button"];
+        if (index === 0) {
+            cls.push('btn-dark');
+            cls.push('active');
+        } else {
+            cls.push('btn-light');
+        }
+        cls.push('btn-block');
+        let imgName = item[0];
+        if (imgName === 'DIVIDER') {
+            $(buttonGroupDiv).append($("<button>", {
+                "type": "button",
+                "class": cls.join(" ")
+            }).prop("disabled", true).text(" "));
+            return;
+        }
+        let caption = item[captionIndex];
+        caption = caption.replace('x', '\u00D7');
+        let imgTag = $("<img>", {'src': PREFIX + imgName, 'id': 'right-img-' + index.toString()});
+        if (index !== 0) {
+            $(imgTag).hide();
+        }
+        $(rightsDiv).append(imgTag);
+        let caption_pre = caption.split('(')[0];
+        let caption_post = caption.split('(')[1];
+        let button = $("<button>", {
+            "type": "button",
+            "class": cls.join(" "),
+            "id": 'right-btn-idx-' + index.toString()}).text(caption_pre);
+        if (caption_post) {  // Not for original
+            let span = $('<span>', {'class': 'codec-info'}).text('(' + caption_post);
+            button.append(span);
+        }
+        button.on("click", function (e) {
+            let oldActiveButton = $(".right-sel-button.active")[0];
+            let oldIndex = oldActiveButton.id.replace('right-btn-idx-', '');
+            let newActiveButton = e.target;
+            if ($(newActiveButton).hasClass('codec-info')) {
+                newActiveButton = $(newActiveButton).parent();
+            }
+            let newIndex = index.toString();
+            if (oldIndex === newIndex) {
+                console.log('Already selected!');
+                return;
+            }
+            /// Switch images
+            $('#right-img-' + newIndex).show();
+            $('#right-img-' + oldIndex).hide();
+            /// First disable old button
+            $(oldActiveButton).removeClass('active');
+            $(oldActiveButton).removeClass('btn-dark');
+            $(oldActiveButton).addClass('btn-light');
+            /// Set new button
+            $(newActiveButton).addClass('active');
+            $(newActiveButton).removeClass('btn-light');
+            $(newActiveButton).addClass('btn-dark');
+            console.log(e.target);
+
+            let compSlider = $(".comparison-slider");
+            let selectorWidth = parseFloat($(".right-selector").css("width"));
+            let divider = $(compSlider).find(".divider");
+            let parentWidth = parseFloat(divider.parent().css("width"));
+            let dividerPos = parseFloat(divider.css("left"));
+            let dividerOverlapsSelector = dividerPos > (parentWidth - selectorWidth);
+            if (dividerOverlapsSelector) {
+                let nonOverlapPos = parentWidth - selectorWidth - 30;
+                console.log(divider.css("left"), selectorWidth);
+                let resizer = $(compSlider).find(".resize");
+                divider.css("left", nonOverlapPos);
+                resizer.css("width", nonOverlapPos);
+            }
+        });
+        $(buttonGroupDiv).append(button);
+    });
+}
+
+function showImgAndUpdateUI(imgName, force) {
+    currentImage = imgName;
+    console.log('Showing', imgName);
+    let imgs = known[imgName];
+    let right = imgs['right'];
+    showLeftImg(imgs['left'], imgs['imginfo'], imgs['fullres'], imgs['loadingRatio']);
+    showRights(imgName, right, force);
+}
+function drags(dragElement, resizeElement, container) {
+    let touched = false;
+    window.addEventListener('touchstart', function () { touched = true; });
+    window.addEventListener('touchend', function ()   { touched = false; });
+    dragElement.on("mousedown touchstart", function (e) {
+        dragElement.addClass("draggable");
+        resizeElement.addClass("resizable");
+        let startX = e.pageX ? e.pageX : e.originalEvent.touches[0].pageX;
+        let dragWidth = dragElement.outerWidth();
+        let posX = dragElement.offset().left + dragWidth - startX;
+        let containerOffset = container.offset().left;
+        let containerWidth = container.outerWidth();
+        let minLeft = containerOffset + 10;
+        let maxLeft = containerOffset + containerWidth - dragWidth - 10;
+        dragElement.parents().on("mousemove touchmove", function (e) {
+            if (touched === false) { e.preventDefault(); }  // Prevent selection.
+            let moveX = e.pageX ? e.pageX : e.originalEvent.touches[0].pageX;
+            let leftValue = moveX + posX - dragWidth;
+            leftValue = Math.min(Math.max(leftValue, minLeft), maxLeft);  // Clip
+            let widthValue = ((leftValue + dragWidth / 2 - containerOffset) / containerWidth * 100) + "%";
+            $(".draggable").css("left", widthValue).on("mouseup touchend touchcancel", function () {
+                $(this).removeClass("draggable");
+                resizeElement.removeClass("resizable");
+            });
+            $(".resizable").css("width", widthValue);
+        }).on("mouseup touchend touchcancel", function () {
+            dragElement.removeClass("draggable");
+            resizeElement.removeClass("resizable");
+        });
+    }).on("mouseup touchend touchcancel", function (e) {
+        dragElement.removeClass("draggable");
+        resizeElement.removeClass("resizable");
+    });
+}
